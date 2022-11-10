3fe3f11193d5c2f845c2f6cc409aeb94d224bab7
Caio Marcatti <caio.marcatti@rb.com.br>
2022-11-10 08:16:41 -0300

    feature check module is enabled

--- src/QueueConsumerServer.php
+++ src/QueueConsumerServer.php
@@ -7,6 +7,8 @@ use CaioMarcatti12\Core\Factory\Annotation\Autowired;
 use CaioMarcatti12\Core\Factory\InstanceFactory;
 use CaioMarcatti12\Core\Factory\Invoke;
 use CaioMarcatti12\Core\Launcher\Annotation\Launcher;
+use CaioMarcatti12\Core\Modules\Modules;
+use CaioMarcatti12\Core\Modules\ModulesEnum;
 use CaioMarcatti12\Core\Validation\Assert;
 use CaioMarcatti12\Data\Request\Objects\Body;
 use CaioMarcatti12\QueueManager\Objects\RoutesQueue;
@@ -23,6 +25,9 @@ class QueueConsumerServer
 
     public function run(): void
     {
+        if(!Modules::isEnabled(ModulesEnum::QUEUE_MANAGER)) return;
+
+
         $queue = $this->argvParser->get('queue', '');
 
         $route = RoutesQueue::getRoute($queue);
